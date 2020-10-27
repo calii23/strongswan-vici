734db27922fbb7d08d33fc04b45fd847daf29eda
Maximilian Schelbach <maximilian@schelbach.de>
2020-10-27 13:28:20 +0100

    improved doc parser tool

--- doc-to-types.js
+++ doc-to-types.js
@@ -9,27 +9,49 @@ function readFromStdin(callback) {
 }
 
 /**
- * @param {string} key
+ * @param {string} value
  * @return {string}
  */
-function toTsKey(key) {
-  if (key.startsWith('<') && key.endsWith('>')) {
-    return `[${key.substring(1, key.length - 1).replace(/[ -/]./g, substring => substring.charAt(1).toUpperCase())}: string]`;
-  }
+function toCamelCase(value) {
+  return value.replace(/[ -/<>]./g, substring => substring.charAt(1).toUpperCase());
+}
 
-  if (key.includes('-') || key.includes(' ')) {
-    return `'${key}'`;
-  }
+/**
+ * @param {string} value
+ * @return {string}
+ */
+function toPascalCase(value) {
+  return value.replace(/([ -/<>]|^)./g, substring => substring
+      .charAt(substring.length - 1)
+      .toUpperCase());
+}
 
-  return key;
+/**
+ * @param {string} key
+ * @return {string[]}
+ */
+function toTsKeys(key) {
+  if (key.endsWith('*'))
+    key = key.substring(0, key.length - 1);
+
+  if (key.startsWith('<') && key.endsWith('>'))
+    return [`[${toCamelCase(key.substring(1, key.length - 1))}: string]`];
+
+  const keys = key.split(', ');
+
+  if (key.includes('-') || key.includes(' '))
+    return keys.map(key => `'${key}'`);
+
+  return keys;
 }
 
 /**
  * @param {string[]} data
  * @param {number} from
+ * @param {string[]} extraInterfaces
  * @return {[string, number]}
  */
-function parseSection(data, from) {
+function parseSection(data, from, extraInterfaces) {
   let i = from;
   let result = '{';
 
@@ -42,6 +64,8 @@ function parseSection(data, from) {
       throw new Error(`Invalid line in section: ${currentLine} (line: ${i + 2})`);
     }
 
+    const tsKeys = toTsKeys(key);
+
     let valueType;
     let docs;
 
@@ -51,19 +75,41 @@ function parseSection(data, from) {
       docs = docs.substring(1, docs.length - 1);
       i++;
     } else if (value.startsWith('{')) {
-      const [section, end] = parseSection(data, i + 1);
+      const [section, end] = parseSection(data, i + 1, extraInterfaces);
       i = end;
-      valueType = section;
+      if ((key.endsWith('*') && !key.includes('<')) || tsKeys.length > 1) {
+        const interfaceName = 'Raw' + toPascalCase(key.split(',', 2)[0]
+            .replace('*', ''));
+        extraInterfaces.push(`interface ${interfaceName} ${section}`);
+        valueType = interfaceName;
+
+        if (key.endsWith('*')) {
+          valueType += ' | ' + interfaceName + '[]';
+        }
+      } else {
+        valueType = section;
+      }
       docs = value.includes('#') ? value.substring(value.indexOf('#') + 2) : null;
     } else {
       valueType = 'string';
       docs = value.substring(1, value.length - 1);
+
+      const enumSearchResult = /\w+(?:\|\w+)+$/.exec(docs);
+      if (enumSearchResult) {
+        valueType = enumSearchResult[0]
+            .split('|')
+            .map(constant => `'${constant}'`)
+            .join(' | ');
+      }
     }
 
-    if (docs) {
-      result += `\n/**\n * ${docs}\n */`;
+    for (const tsKey of tsKeys) {
+      if (docs) {
+        result += `\n/**\n * ${docs}\n */`;
+      }
+
+      result += `\n${tsKey}: ${valueType};`;
     }
-    result += `\n${toTsKey(key)}: ${valueType};`;
   }
 
   result += '\n}';
@@ -77,10 +123,14 @@ function parseSection(data, from) {
  */
 function toInterface(data) {
   const lines = data.split('\n')
-      .map(value => value.trim());
+      .map(value => value.trim())
+      .filter(value => value);
 
   lines.shift();
   lines.pop();
 
-  return 'interface Data ' + parseSection(lines, 0)[0];
+  const interfaces = [];
+  interfaces.push('interface RawData ' + parseSection(lines, 0, interfaces)[0]);
+
+  return interfaces.join('\n\n');
 }
